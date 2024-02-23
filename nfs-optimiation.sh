287676390eb8af906647a7699be545697ceb008e
nixCraft <vivek@nixcraft.com>
2024-02-23 17:54:43 +0530

    Update nfs-optimiation.sh

--- nfs-optimiation.sh
+++ nfs-optimiation.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# See how to hook this script using the systemd https://www.cyberciti.biz/faq/how-to-run-script-after-mounting-nfs-with-systemd-in-linux/
+# SysV user just call this script from the /etc/rc.local
 device_number=$(stat -c '%d' /cbz_efs/)
 ((major = (device_number & 0xFFF00) >> 8))
 ((minor = (device_number & 0xFF) | ((device_number >> 12) & 0xFFF00)))
